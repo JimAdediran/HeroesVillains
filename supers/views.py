f27f443be7be6698d0fe8bd5f197fdac31745b03
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-07-22 17:47:26 -0400

    code updated but post in postman giving error +  custom dictionary get all in postman giving error

--- supers/views.py
+++ supers/views.py
@@ -3,20 +3,52 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+import super_types
 from supers.serializers import SupersSerializer
+from super_types.serializers import SuperTypeSerializer
 from .models import Supers
+from super_types.models import SuperType
 from supers import serializers
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    if request.method == 'POST':
+    supers = Supers.objects.all()
+    custom_response_dict = {}
+    if request.method == 'GET':
+        super_type_name = request.query_params.get('super_type')
+        print(super_type_name)
+        queryset = Supers.objects.all()
+
+        if super_type_name:
+            queryset = queryset.filter(super_type__type=super_type_name)
+            serializer = SupersSerializer(queryset, many=True)
+            return Response(serializer.data)
+        else: 
+            for super in supers:
+                super_types = Supers.objects.filter("Hero")
+                hero_serializer = SupersSerializer(super_types, many=True)
+                super_types2 = Supers.objects.filter("Villain")
+                villain_serializer = SupersSerializer(super_types2, many=True)
+                custom_response_dict = {
+                    "Heroes": hero_serializer.data,
+                    "Villains": villain_serializer.data
+                }
+            return Response(custom_response_dict)
+    elif request.method == 'POST':
         serializer = SupersSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+        
+        
+            
+    
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
     supers = get_object_or_404(Supers, pk=pk)
     if request.method == 'GET':
@@ -27,5 +59,8 @@ def supers_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        supers.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
